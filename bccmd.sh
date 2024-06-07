ccef66bd1f4bb737db0b3da73c523227fce323b7
Mutsuto Yamauchi <141827050+yamauchiio@users.noreply.github.com>
2024-06-07 16:11:20 +0900

    Update bccmd.sh

--- bccmd.sh
+++ bccmd.sh
@@ -13,15 +13,30 @@ git clone -b 5.55 https://git.kernel.org/pub/scm/bluetooth/bluez.git
 # クローンしたディレクトリに移動
 cd bluez
 
+# g_memdup を g_memdup2 に置き換える
+sed -i 's/g_memdup(/g_memdup2(/g' plugins/policy.c
+
+# pause関数の名前を変更する
+sed -i 's/static bool pause/static bool bluetoothd_pause/g' profiles/audio/media.c
+sed -i 's/pause(/bluetoothd_pause(/g' profiles/audio/media.c
+
 # ビルドの準備
 ./bootstrap
 
-# bccmd を含むすべてのツールをビルド
+# bccmd および hid2hci を含むすべてのツールをビルド
 ./configure
 make
 
-# bccmd をインストール
+# bccmd および hid2hci をインストール
 sudo make install
+
+# インストール先を確認
+if [ -f /usr/local/bin/hid2hci ]; then
+  echo "hid2hci is successfully installed in /usr/local/bin"
+else
+  echo "Error: hid2hci is not found in /usr/local/bin"
+  exit 1
+fi
 
 # パスを通す
 echo 'export PATH=$PATH:/usr/local/bin' >> ~/.bashrc
@@ -30,4 +45,4 @@ echo 'export PATH=$PATH:/usr/local/bin' >> ~/.bashrc
 source ~/.bashrc
 
 # 作業完了メッセージ
-echo "BlueZ 5.55 の bccmd がビルドおよびインストールされ、パスが設定されました。"
+echo "BlueZ 5.55 の bccmd および hid2hci がビルドおよびインストールされ、パスが設定されました。"
